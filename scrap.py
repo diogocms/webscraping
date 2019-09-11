58d4191dfc23ad1996bedfabfd834a0fc1fd517e
DiogoMesquita <cms.diogo@gmail.com>
2019-09-11 10:01:29 -0300

    first commit

---
+++ scrap.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+class cm_scraper():
+
+  def __init__(self, year_link):
+    self.year_link = year_link
+    self.brands = []
+
+  def scrap(self):
+    start = time.time()
+
+    #listing link for brands
+    page = requests.get(self.year_link)
+    if page.status_code != 200:
+      print("An error has ocurred in the main link")
+    page_response = page.content
+    page_content = BeautifulSoup(page_response, 'lxml')
+    page_tags = page_content.find_all(class_="item-marca")
+    page_links = []
+
+    for page_tag in page_tags:
+      page_links.append(page_tag['href'])
+
+
+    #getting images by brand
+    brands = []
+
+    for link in page_links:
+      brand_page = requests.get(link)
+      if brand_page.status_code == 200:
+        brand_response = brand_page.content
+        brand_content = BeautifulSoup(brand_response, 'lxml')
+
+        brand_pictures = []
+        brand_name = brand_content.h1.string
+        brand_tags = brand_content.find_all(class_="cont-foto")
+
+        for tag in brand_tags:
+          brand_pictures.append(tag.contents[1]["src"])
+
+        brand = {'name': brand_name , 'url': link, 'links':brand_pictures}
+        brands.append(brand)
+      else:
+        print("An error has ocurred in one of the brands")
+        break
+
+
+    end = time.time()
+    time_stamp = round((end-start)/60, 4)
+
+    print(f'Scraped in {time_stamp} minutes')
+    print(f'{len(brands)} brands scraped')
+    print()
+
+    for brand in brands:
+      print(brand["name"])
+
+    self.brands = brands
---
+++ teste.py
@@ -0,0 +1,5 @@
+from scrap import cm_scraper
+
+meu_scraper = cm_scraper("https://fimi.es/en/galeriaspasarela/public/marcas?id=14&idferia=1&f=4")
+
+meu_scraper.scrap()
